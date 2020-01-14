7626295e2efe5f27d34564df7d445d701b191aeb
LenaTarashchik <lenchikk.t@gmail.com>
2020-01-14 18:17:32 +0200

    add task6,7/16

---
+++ preparation/Task4/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//           Пользователь вводит число 1 или 0 с помощью одной строки кода
+//вывести на экран «Хорошо» если пользователь ввел 1 или «Плохо»
+//если пользователь ввел 0
+            
+            Console.WriteLine("Введите 0 или 1");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x==0 ? "Плохо" : "Хорошо");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ preparation/Тask6,7/Тask6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*            Служба такси заказала вам программу, которая спрашивает количество
+километров и количество минут простоя, а дальше считает так: первые
+5 километров (или до 5 км) 20 гривен, каждый следующий километр 3
+гривны, плюс простой 1 грн за 1 минуту. Программа должна посчитать
+и сказать общую сумму оплаты за поездку.*/
+
+            double grn;
+            int a = 20; // цена за первые 5 км или до 5
+            Console.WriteLine("Введите количество километров:");
+            double km = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите количество минут простоя:");
+            double hv = Convert.ToDouble(Console.ReadLine());
+            if (km <= 5)
+            {
+                grn = a + hv;
+            }
+            else
+            {
+                grn = a + (km - 5) * 3 + hv;
+            }
+            Console.WriteLine("Общая сумма:" + grn);
+            Console.ReadLine();
+
+//            Дано натуральное число. Выяснить, является ли оно простым, т.е.
+//делится только на 1 и на само себя.
+            Console.WriteLine("Введите натуральное число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int r=0;
+            if (n > 1)
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    double ost;
+                    ost = n % i;
+                    if (ost == 0) r++;
+                }
+                if (r == 1)
+                {
+                    Console.WriteLine("Простое");
+                }
+                else
+                {
+                    Console.WriteLine("Составное");
+                }
+            }
+            else Console.WriteLine("Число 1 не относится ни к простым, ни к составным числам");
+            Console.ReadLine();
+        }
+    }
+}
